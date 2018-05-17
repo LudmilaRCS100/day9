10f9010ee2c2896aa7050270fffc76f32c50c3fb
Ludmila <luvi_lv@inbox.lv>
2018-05-17 20:09:17 +0300

    9.2

---
+++ day9/day9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test TestaObjekts = new Test();
+            //masina ford = new Test();
+            int rezultats = TestaObjekts.Saskaitisana(3, 7);
+            Test TestaObjekts2 = new Test();
+
+            TestaObjekts.skaitlis = 3;
+            TestaObjekts2.skaitlis = 6;
+
+            Console.WriteLine("Izvade no TestaObjekta: " + TestaObjekts.skaitlis);
+            Console.WriteLine("Izvade no TestaObjekta2: " + TestaObjekts2.skaitlis);
+            Console.WriteLine("Izvade no klases: " + rezultats);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ day9/day9/Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day9
+{
+    class Test
+    {
+        public int skaitlis;
+        public int Saskaitisana(int a, int b)
+        {
+            Izvade();
+            return a + b;
+        }
+
+        private void Izvade()
+        {
+            Console.WriteLine("tests!");
+        }
+        
+
+        
+        
+
+
+        
+
+    }
+
+       
+    
+
+        
+        
+
+        
+}
